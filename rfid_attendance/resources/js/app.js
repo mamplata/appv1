e732fb71ab2825ae8daa7e8071d0be3f7945f115
Mamplata, Kyle Deejay C. <109187013+mamplata@users.noreply.github.com>
2025-01-30 23:57:18 +0800

    Update app.js

--- rfid_attendance/resources/js/app.js
+++ rfid_attendance/resources/js/app.js
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Link } from '@inertiajs/vue3';  // Import Link component
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +20,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('Link', Link)  // Register Inertia's Link component globally
             .mount(el);
     },
     progress: {
